d6371af74f658bdcd6a942b7a8bde074bec0ba8b
Latif Bolum <latifbolum@gmail.com>
2021-03-14 17:40:07 +0100

    clening

--- main/yahoo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-commodity_choices = (
-    ("SILVER", "SILVER"),
-    ("GOLD", "GOLD"),
-)
-
-class CommoditiesForm(forms.Form):
-    commodity_field = forms.MultipleChoiceField(choices = commodity_choices)
